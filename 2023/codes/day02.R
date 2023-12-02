616b513e908e3d3fd1c2ac4b665d021b3c76892c
Thomas Vroylandt <tvroylandt@gmail.com>
2023-12-02 06:20:00 +0100

    Day 02 part one ok

---
+++ 2023/codes/day02.R
@@ -0,0 +1,43 @@
+# ---------------- #
+# Day 01 #
+# ---------------- #
+
+library(tidyverse)
+
+# Part one ----------------------------------------------------------------
+
+# get the right format
+df_bag <- 
+  tibble(string = readLines("2023/inputs/day02.txt")) |>
+  separate(string, into = c("id", "sets"), sep = ": ") |>
+  separate_longer_delim(sets, delim = ";") |>
+  group_by(id) |>
+  mutate(sets_nb = row_number()) |>
+  ungroup() |>
+  separate_longer_delim(sets, delim = ",") |>
+  mutate(sets = str_trim(sets),
+         id = as.numeric(str_remove(id, "Game "))) |>
+  separate(sets, into = c("nb", "color"), sep  = " ") |>
+  pivot_wider(
+    names_from = color,
+    values_from = nb,
+    values_fill = list(nb = 0),
+    values_fn = as.numeric
+  )
+
+# compare
+df_bag_filter <- df_bag |> 
+  filter(!(green <= 13 & red <= 12 & blue <=14)) |> 
+  distinct(id)
+
+# remove
+df_bag_id <- df_bag |> 
+  filter(!id %in% df_bag_filter$id) |> 
+  distinct(id)
+
+# answer
+sum(df_bag_id$id)
+
+# 2377
+
+# Part two ----------------------------------------------------------------
